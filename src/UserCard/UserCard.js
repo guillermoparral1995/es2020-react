7060ffef08c3e036f08d48a4fe5b063fd9477318
Guillermo Parral <guillermo.parral@mercadolibre.com>
2020-03-04 15:07:27 -0300

    add some magic

--- src/UserCard/UserCard.js
+++ src/UserCard/UserCard.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './styles.scss';
 
-const UserCard = ({ user }) => <div className='user-card'>
-  <p>{`${user.name} ${user.surname}`}</p>
-</div>
+
+const UserCard = ({ user }, suggestion) => {
+
+  const hasName = (user) => user.name ? user : throw Error('User doesn´t have name!');
+  const likesMovies = (user) => user.favorites.movies ? user : throw Error('User doesn´t like movies!');
+  const addDowntonAbbey = (user) => user.favorites.movies = [...user.favorites.movies, 'Downton Abbey'];
+  const sayGoodbye = (user) => {
+    console.log(`Goodbye! ${user.name}`);
+    return user;
+  }
+
+  const validatedUser = user
+    |> hasName
+    |> likesMovies
+    |> addDowntonAbbey
+    |> sayGoodbye
+
+  return <div className='user-card'>
+    <p>{`${user.name} ${user.surname}`}</p>
+  </div>
+}
 
 export default UserCard
